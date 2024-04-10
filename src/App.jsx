c05eafd06324c7a54b00ebcd6a6a85b5fadba193
francescomerighi1202 <francescomerighi61@gmail.com>
2024-04-10 10:04:24 +0200

    Inizializzazione progetto

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./styles/App.css";
+
+import Home from "./components/Home";
+import Languages from "./components/Languages";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Home />
+            </>
+          }
+        />
+        <Route
+          path="/languages"
+          element={
+            <>
+              <Header />
+              <Languages />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
