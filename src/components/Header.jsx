823c805c7ef889b225d0b34b8d5534178e9bb231
francescomerighi1202 <francescomerighi61@gmail.com>
2024-04-12 10:28:33 +0200

    Sviluppo mobile layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,11 @@ import { Link } from "react-router-dom";
 function Header() {
 
   const [isActive, setIsActive] = React.useState("");
+  const [menuOpen, setMenuOpen] = React.useState(false);
+
+  function toggleMenu() {
+    setMenuOpen(!menuOpen);
+  }
 
   return (
     <header>
@@ -19,6 +24,11 @@ function Header() {
           <h1>Translator App</h1>
         </div>
         <nav className="header-menu">
+          <div className="hamburger-menu" onClick={toggleMenu}>
+            <div className={menuOpen ? "line active" : "line"}></div>
+            <div className={menuOpen ? "line active" : "line"}></div>
+            <div className={menuOpen ? "line active" : "line"}></div>
+          </div>
           <ul>
             <li>
               <Link
@@ -49,6 +59,37 @@ function Header() {
             </li>
           </ul>
         </nav>
+        {menuOpen && (
+          <ul className="menu-mobile">
+            <li>
+              <Link
+                to="/translate"
+                onClick={() => setIsActive("Translate")}
+                className={isActive === "Translate" ? "link-active" : null}
+              >
+                Traduci
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/detect"
+                onClick={() => setIsActive("Detect")}
+                className={isActive === "Detect" ? "link-active" : null}
+              >
+                Rileva lingua
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/languages"
+                onClick={() => setIsActive("Languages")}
+                className={isActive === "Languages" ? "link-active" : null}
+              >
+                Lingue supportate
+              </Link>
+            </li>
+          </ul>
+        )}
       </Container>
     </header>
   );
